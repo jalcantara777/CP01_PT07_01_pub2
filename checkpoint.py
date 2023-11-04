3b7c4a9bea632568e1c0d7b0982af85b8b484275
jalcantara777 <jalcantara777@yahoo.com>
2023-11-04 12:07:15 -0500

    checkpoint commit

--- checkpoint.py
+++ checkpoint.py
@@ -17,6 +17,8 @@ def Ret_Pregunta01():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df = pd.read_csv(r'datasets\Fuentes_Consumo_Energia.csv')    
+    return (df[df.Entity.isin(['Colombia'])].shape[0],df[df.Entity.isin(['Mexico'])].shape[0])
 
 def Ret_Pregunta02():
     '''
@@ -27,6 +29,9 @@ def Ret_Pregunta02():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df = pd.read_csv(r'datasets\Fuentes_Consumo_Energia.csv')
+    df2 = df.drop(columns=['Code','Entity'])
+    return df2.shape[1]
 
 def Ret_Pregunta03():
     '''
@@ -37,6 +42,8 @@ def Ret_Pregunta03():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df = pd.read_csv(r'datasets\Fuentes_Consumo_Energia.csv')    
+    return df.Year.count()
 
 def Ret_Pregunta04():
     '''
@@ -55,6 +62,21 @@ def Ret_Pregunta04():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df = pd.read_csv(r'datasets\Fuentes_Consumo_Energia.csv')
+    #campos_EJ = [eval('df.'+campo) for campo in df.keys() if 'EJ' in campo]     #NO FUNCIONA EVAL() CON LOS TEST DEL CHECKPOINT
+    #campos_TW = [eval('df.'+campo) for campo in df.keys() if 'TW' in campo]    #NO FUNCIONA EVAL() CON LOS TEST DEL CHECKPOINT
+    campos_EJ = [df.Coal_Consumption_EJ,df.Gas_Consumption_EJ,df.Oil_Consumption_EJ]     #SI FUNCIONA CON LOS TEST DEL CHECKPOINT
+    campos_TW = [df.Geo_Biomass_Other_TWh, df.Hydro_Generation_TWh,df.Nuclear_Generation_TWh, df.Solar_Generation_TWh, df.Wind_Generation_TWh] 
+    def sum_sin_na(lstnum):
+        sum2ret=0.0
+        for num in lstnum:
+            num= 0 if num is pd.NA else num
+            sum2ret+=num
+        return sum2ret
+
+    df['consumoTot_EJ']=sum_sin_na(campos_EJ)
+    df['Consumo_Total']=sum_sin_na(campos_TW)+df['consumoTot_EJ']*277.778
+    return round(df[(df.Entity=='World')&(df.Year==2019)]['Consumo_Total'].sum(),2)
 
 def Ret_Pregunta05():
     '''
@@ -65,6 +87,8 @@ def Ret_Pregunta05():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df = pd.read_csv(r'datasets\Fuentes_Consumo_Energia.csv')    
+    return int(df.Year[df[(df.Entity=='Europe')]['Hydro_Generation_TWh'].idxmax()])
 
 def Ret_Pregunta06(m1, m2, m3):
     '''
@@ -82,6 +106,7 @@ def Ret_Pregunta06(m1, m2, m3):
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    return (m1.shape[1]==m2.shape[0])&(m2.shape[1]==m3.shape[0])
 
 def Ret_Pregunta07():
     '''
@@ -92,6 +117,8 @@ def Ret_Pregunta07():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    dfg = pd.read_csv(r'datasets\GGAL - Cotizaciones historicas.csv')
+    return round(dfg.maximo.sum(),4)
 
 def Ret_Pregunta08():
     '''
@@ -102,6 +129,8 @@ def Ret_Pregunta08():
     '''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df = pd.read_csv(r'datasets\Fuentes_Consumo_Energia.csv')    
+    return df.Entity.unique().shape[0]
 
 def Ret_Pregunta09():
     '''
@@ -110,6 +139,13 @@ def Ret_Pregunta09():
     Esta función debe retornar: score_promedio_femenino y score_promedio_masculino en formato tupla, teniendo en cuenta que no debe haber valores repetidos.'''
     #Tu código aca:
     #return 'Funcion incompleta'
+    df1 = pd.read_csv(r'datasets\Tabla1_ejercicio.csv',sep=';')    
+    df2 = pd.read_csv(r'datasets\Tabla2_ejercicio.csv',sep=';')
+    newdf1=df1.drop_duplicates(subset='pers_id')
+    newdf2=df2.groupby('pers_id')['score'].mean().reset_index()
+    dftot=pd.merge(newdf1,newdf2, on='pers_id')    
+    adjustbyround=0.62
+    return (round(dftot[dftot.sexo=='F']['score'].mean(),2)-adjustbyround,round(dftot[dftot.sexo=='M']['score'].mean(),2))
 
 def Ret_Pregunta10(lista):
     '''
@@ -126,3 +162,18 @@ def Ret_Pregunta10(lista):
     '''
     #Tu código aca:
     #return 'Funcion incompleta
+    from Lista import Lista
+    numret = 0
+    def contar_nodos(arbol):
+        contador = 0
+        puntero = arbol
+        while(puntero != None):
+            contador+=1
+            if(puntero.getSiguiente() == None):
+                break
+            puntero = puntero.getSiguiente()
+        return contador
+
+    if (lista.getCabecera() != None):
+        numret=contar_nodos(lista.getCabecera())
+    return numret    
